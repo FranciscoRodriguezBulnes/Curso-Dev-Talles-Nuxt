42ffdfdebf3fbb5887a5e17922425674b82fdcd7
Francisco Rodríguez Bulnes <158209260+FranciscoRodriguezBulnes@users.noreply.github.com>
2026-03-12 10:25:24 +0100

    Sección 6

--- seed/seed-database.ts
+++ seed/seed-database.ts
@@ -1,16 +1,22 @@
-import prisma from "../lib/prisma.ts";
-import { siteReviews } from "./site-reviews.seed.ts";
+import prisma from '../lib/prisma.ts';
+import { siteReviews } from './site-reviews.seed.ts';
+import { products } from './products.seed.ts';
 
 async function seedDatabase() {
   // Purgar base de datos
   await prisma.siteReview.deleteMany();
+  await prisma.product.deleteMany();
 
   // Insertar registros
   await prisma.siteReview.createMany({
     data: siteReviews,
   });
 
-  console.log("Database seeded successfully");
+  await prisma.product.createMany({
+    data: products,
+  });
+
+  console.log('Database seeded successfully');
 }
 
 seedDatabase();
